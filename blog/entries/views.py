3c71f4147fea1b18ff7924ae05d87630d931e3e0
Abhishek5101 <miabhishekkulkarni@gmail.com>
2019-12-12 11:27:40 -0800

    users app created

--- blog/entries/views.py
+++ blog/entries/views.py
@@ -7,6 +7,8 @@ class HomeView(ListView):
 	model = Entry
 	template_name = 'entries/index.html'
 	context_object_name = 'blog_entries'
+	ordering = ['-entry_date']
+	paginate_by = 4
 
 
 class EntryView(DetailView):
@@ -17,4 +19,8 @@ class EntryView(DetailView):
 class CreateEntryView(CreateView):
 	model = Entry
 	template_name = 'entries/create_entry.html'
-	fields = ['title', 'text']
+	fields = ['entry_title', 'entry_text']
+	
+	def form_valid(self, form):
+		form.instance.entry_author = self.request.user.id
+		return super().form_valid(form)
